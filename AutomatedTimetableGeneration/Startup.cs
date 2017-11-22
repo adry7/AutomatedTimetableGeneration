80e51ef5a50891b09b79bbaa4e1f584313e5e0e5
Mustafa Adry <mustafa.adry2025@gmail.com>
2017-11-22 09:32:53 +0200

    Add project files.

---
+++ AutomatedTimetableGeneration/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AutomatedTimetableGeneration.Startup))]
+namespace AutomatedTimetableGeneration
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
